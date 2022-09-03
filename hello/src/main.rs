57f77288cf6a671585ec9fd10a792811c97dca93
kakts <kakts.git@gmail.com>
2022-09-04 00:15:35 +0900

    2.4: pass arg value from commnad line

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,9 +1,45 @@
+use std::str::FromStr;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
-    println!("gcd(10,2): {}", gcd (10, 2));
+
+    // 可変なローカル変数numbers 空のベクタで初期化
+    // Vecはサイズ可変のベクタ型 jsだと配列に相当する
+    // 数をベクタの最後に追加できるようにするには変数にmutをつける必要が亜rう
+    // この後の処理で Vec<u64>となるが、ここでは書かない 
+    let mut numbers = Vec::new();
+
+    /**
+     *コマンドライン引数の処理 args()はイテレータを返す。
+     *
+     */
+    for arg in env::args().skip(1) {
+        // コマンドラインで受け取った文字列をu64に変換
+        // 型u64はFromStrを実装しているので、u64::from_strを読んでコマンドライン引数を解釈できる
+        // ここでu64をプッシュしているのでRustはこのベクタの型を推論できる
+        // from_strはResultを返す
+        // Resultは Ok(v)かErr(e)を返す
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    // 引数の指定がない場合
+    if numbers.len() == 0 {
+        // eprintln! stderrに出力
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
 /**
+ * 
+ * 最大公約数を計算する
  * u64 符号なし64ビット整数
  * 
  * mutキーワードをつけると再代入可能
