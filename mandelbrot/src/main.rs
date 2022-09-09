cdc2a0571b13a64c9c8f8178ed25a9f02e5bd120
kakts <kakts.git@gmail.com>
2022-09-10 01:24:12 +0900

    2.6.7: mandelbrot call render function as parallel

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -3,12 +3,55 @@ use num::Complex;
 use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
+use std::env;
+
 
 fn main() {
-    println!("Hello, world!");
 
-    let parsed_complex = parse_complex("1.25,-0.0625");
-    println!("parsed complex result is {:?}", parsed_complex);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    // 並列化されていないバージョン
+    // render(&mut pixels, bounds, upper_left, lower_right);
+
+    // crossbeamクレートによる並列化
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+
+        // クロージャ
+        // crossbeam::scopeは、全ての生成されたスレッドが終了するのを待ってから終了する
+        // プログラマにとっては、corssbeam::scopeがリターンしてきたら、画像の計算が終了していることが保証される
+        crossbeam::scope(|spawner| {
+            // クロージャないで新しいスレッドを生成する
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        }).unwrap();
+    }
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 /// 大きさがbounds で指定されたバッファpixelsをfilenameで指定されたファイルに書き出す
@@ -22,7 +65,7 @@ fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<
     };
     let encoder = PNGEncoder::new(output);
 
-    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8));
 
     Ok(())
 }
@@ -100,12 +143,12 @@ fn pixel_to_point(bounds: (usize, usize),
 
 fn render(pixels: &mut [u8],
             bounds: (usize, usize),
-            upper_left: Complex<f64>
-            lower_right: Complex<f64
+            upper_left: Complex<f64>,
+            lower_right: Complex<f64>,
 ) {
     assert!(pixels.len() == bounds.0 * bounds.1);
 
-    for now in 0..bounds.1 {
+    for row in 0..bounds.1 {
         for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row),
                 upper_left, lower_right);
