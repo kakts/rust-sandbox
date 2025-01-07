e022d0860652cba96e6ce85ab642da447cd64c47
kakts <kakts.git@gmail.com>
2025-01-08 00:27:55 +0900

    [guessing_game] made up guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Hello, world!");
@@ -7,9 +8,41 @@ fn main() {
 
     let mut guess = String::new();
 
-    // stdinからの入力を受け取る
-    // Result.expect()は、ResultがErrの場合に引数のメッセージを表示してプログラムをクラッシュさせる
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    let rand_num = get_random_num();
+    let mut count = 0;
+    loop {
+        // stdinからの入力を受け取る
+        // Result.expect()は、ResultがErrの場合に引数のメッセージを表示してプログラムをクラッシュさせる
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {}", guess);
+        let guess_num: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(e) => {
+                println!("Please input a valid number! {}", e);
+                guess.clear();
+                continue;
+            }
+        };
+
+        if guess_num == rand_num {
+            println!("You win! challenge count is : {}", &count);
+            break;
+        } else if guess_num > rand_num{
+            println!("{} is too big!", &guess_num);
+        } else {
+            println!("{} is too small!",&guess_num);
+        }
+        guess.clear();
+        count += 1;
+    }
+
+
+}
+
+
+fn get_random_num() -> i32 {
+
+    let mut rng = rand::thread_rng();
+    return rng.gen_range(1..100);
 }
